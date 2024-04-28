2c27af6c9f3e9a609ebd69c65eb6386b0e5210dc
TredexLOL <79072850+TredexLOL@users.noreply.github.com>
2024-04-28 14:47:12 +1000

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Car
-class CarAdmin(admin.ModelAdmin):
-    list_display = ('mark', 'model', 'year', 'transmission', 'mileage','fuel', 'price', 'date_posted')
-    list_filter = ('mark', 'transmission', 'year')
-    search_fields = ('mark', 'model')
-
-admin.site.register(Car, CarAdmin)
-
